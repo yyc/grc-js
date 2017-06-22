5e2a31c36e6e4a3edc7a26159f4756253e3102e4
Yuchuan <yyc478@gmail.com>
2017-06-22 16:52:17 +0800

    Add GRC info request scraping

--- grc.js
+++ grc.js
@@ -1,11 +1,27 @@
-var Redis = require("ioredis");
-var fetch = require("node-fetch");
+const Redis = require("ioredis");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
 //fetch("https://google.com").then(console.log);
 
 var redis = new Redis();
+fetch.Promise = Promise;
 
-function fetchGRCCDC(postal_code) {}
+// This is called after
+async function fetchGRCCDC(postal_code) {
+  if (!isValidPC(postal_code)) {
+    return Promise.reject();
+  }
+  var result = await fetch(
+    "http://sis.pa-apps.sg/NASApp/sim/SearchResults.jsp",
+    {
+      method: "POST",
+      body: `intEdtPostalCode=${postal_code}`
+    }
+  );
+  $ = await result.text().then(txt => Promise.resolve(cheerio.load(txt)));
+  console.log($("tr").html());
+}
 
 // Just checking if it's a 6-digit number.
 // We can do better, but the rules https://en.wikipedia.org/wiki/Postal_codes_in_Singapore
@@ -15,3 +31,27 @@ function isValidPC(postal_code) {
   var str = (postal_code + "").trim();
   return /^[1-9][0-9]{5}$/.test(str);
 }
+
+function dataFnFor(type) {
+  return async pc => {
+    if (!isValidPC) {
+      return Promise.reject();
+    }
+    var json = await redis.get(pc);
+    if (json == null) {
+      // We need to fetch this from the API
+      await fetchGRCCDC(pc);
+    }
+  };
+}
+
+function allDataFor() {}
+module.exports = {
+  grcFor: dataFnFor("grc"),
+  cdcFor: dataFnFor("cdc"),
+  ssoFor: dataFnFor("sso"),
+  allDataFor,
+  isValidPC
+};
+
+dataFnFor("sso")("659248");
