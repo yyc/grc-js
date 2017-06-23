b9be5017b06c3d862b10a73c5d5768d7a2608ae9
Yuchuan <yyc478@gmail.com>
2017-06-23 10:41:21 +0800

    Able to fetch GRC and CDC data

--- grc.js
+++ grc.js
@@ -7,7 +7,7 @@ const cheerio = require("cheerio");
 var redis = new Redis();
 fetch.Promise = Promise;
 
-// This is called after
+// This is called after a cache miss
 async function fetchGRCCDC(postal_code) {
   if (!isValidPC(postal_code)) {
     return Promise.reject();
@@ -16,11 +16,36 @@ async function fetchGRCCDC(postal_code) {
     "http://sis.pa-apps.sg/NASApp/sim/SearchResults.jsp",
     {
       method: "POST",
-      body: `intEdtPostalCode=${postal_code}`
+      body: `intEdtPostalCode=${postal_code}&flag=a`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Referer: "http://sis.pa-apps.sg/NASApp/sim/AdvancedSeachResults1.jsp",
+        Origin: "http://sis.pa-apps.sg",
+        Host: "sis.pa-apps.sg",
+        Cookie: "dis-request-id=efe4bb17ac1be3ac5cc1e5391d9ed12f",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4",
+        connection: "keep-alive"
+      }
     }
   );
   $ = await result.text().then(txt => Promise.resolve(cheerio.load(txt)));
-  console.log($("tr").html());
+  var cdc = $("tr").eq(5).children("td").eq(1).text().trim();
+  var grc = $("tr").eq(13).children("td").eq(1).text().trim();
+
+  await redis
+    .multi()
+    .hset(postal_code, "cdc", cdc)
+    .hset(postal_code, "grc", grc)
+    .exec();
+  return {
+    cdc,
+    grc
+  };
 }
 
 // Just checking if it's a 6-digit number.
@@ -37,10 +62,14 @@ function dataFnFor(type) {
     if (!isValidPC) {
       return Promise.reject();
     }
-    var json = await redis.get(pc);
-    if (json == null) {
+    var json = await redis.hgetall(pc);
+    if (json == null || Object.keys(json) == 0) {
       // We need to fetch this from the API
-      await fetchGRCCDC(pc);
+      return fetchGRCCDC(pc).then(obj => {
+        obj[type];
+      });
+    } else {
+      return Promise.resolve(json[type]);
     }
   };
 }
@@ -54,4 +83,4 @@ module.exports = {
   isValidPC
 };
 
-dataFnFor("sso")("659248");
+dataFnFor("grc")("138593").then(console.log);
