6abbf047b88d1927d1792329e7a21c7475caf25f
Yuchuan <yyc478@gmail.com>
2017-06-29 12:40:54 +0800

    get /postcodes working

--- routes/index.js
+++ routes/index.js
@@ -32,4 +32,35 @@ router.get("/postcode/:postal_code", function(req, res, next) {
     });
 });
 
+router.post("/postcodes", function(req, res, next) {
+  var postcodes = req.body;
+  if (!Array.isArray(postcodes)) {
+    res.status(500).send({
+      error: 500,
+      message: "Invalid array!"
+    });
+    return;
+  }
+  postcodes = postcodes.filter(GRC.isValidPC);
+  var pcPromises = postcodes.map(postal_code => {
+    return GRC.allDataFor(postal_code).then(allData =>
+      Promise.resolve({
+        postal_code,
+        allData
+      })
+    );
+  });
+  Promise.all(pcPromises).then(responses => {
+    var response = {};
+    responses.forEach(result => {
+      response[result.postal_code] = result.allData;
+    });
+    res.send(response);
+  });
+});
+
+/*router.post('/projects/:filename', function(req, res, next){
+    console.log(req.body);
+    res.render(req.params.filename, req.body);
+})*/
 module.exports = router;
