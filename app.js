eebcb8aebd42f2b2d670f50a8ae8376fe5008be7
Yuchuan <yyc478@gmail.com>
2017-06-29 12:31:34 +0800

    Get /postcode/:pc working

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require("./routes/index");
 
 var app = express();
 
-// view engine setu
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,35 @@
 var express = require("express");
 var router = express.Router();
-var data = require("../lib/grc.js");
+var GRC = require("../lib/grc.js");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-/*router.post('/projects/:filename', function(req, res, next){
-    console.log(req.body);
-    res.render(req.params.filename, req.body);
-})*/
+/* GET postcode */
+router.get("/postcode/:postal_code", function(req, res, next) {
+  var postal_code = req.params["postal_code"];
+  // var data = req.query["data"] || "grc+cdc+sso";
+  if (!GRC.isValidPC(postal_code)) {
+    res.status(500).send({
+      error: 500,
+      message: "Invalid postal code!"
+    });
+    return;
+  }
+
+  GRC.allDataFor(postal_code)
+    .then(allData => {
+      res.send(allData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send({
+        error: 500,
+        message: err
+      });
+    });
+});
 
 module.exports = router;
