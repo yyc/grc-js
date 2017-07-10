b9e93ce7aa1a18d26fe6cc6989e6bca2d12089f9
Yuchuan <yyc478@gmail.com>
2017-07-10 14:42:51 +0800

    Add helper scripts

---
+++ bin/postal_codes.js
@@ -0,0 +1,2 @@
+// Put an array of postal codes here for process.js to..process
+module.exports = [];
---
+++ bin/process.js
@@ -0,0 +1,26 @@
+// Used to do a pass through all the postal codes to build up a cache in redis
+
+var GRC = require("../lib/grc");
+var postal_codes = require("./postal_codes");
+
+async function process(postal_codes) {
+  for (var i = 0; i < postal_codes.length; i++) {
+    let postal_code = postal_codes[i];
+    try {
+      let data = await GRC.allDataFor(postal_code);
+      console.log(`${postal_code},${data.grc},${data.cdc},${data.sso}`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+// Process in 4 queues
+var length = postal_codes.length;
+var piece = Math.floor(length / 4);
+
+for (var i = 0; i < 4; i++) {
+  let slice = postal_codes.slice(i * piece, (i + 1) * piece);
+  console.log(`starting slice ${i} at ${slice[0]}`);
+  process(slice);
+}
