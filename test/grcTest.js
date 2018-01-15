991a3ab110448f0d44200d6cdc1e2e3aa692f70b
Yuchuan <yyc478@gmail.com>
2018-01-15 16:21:57 -0600

    Add additional tests

--- test/grcTest.js
+++ test/grcTest.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const should = require("should");
 const grc = require("./../lib/grc");
 
+var compData = {};
 it("should initiate redis connection correctly", done => {
   should.exist(grc.redis);
   done();
@@ -12,11 +13,11 @@ it("should retrieve SSO for a given postal code", () => {
     .catch(err => {
       console.error(err);
       should.not.exist(err);
-      done();
     })
     .then(ssoName => {
       should.exist(ssoName);
-      done();
+      ssoName.should.not.equal("");
+      compData.sso = ssoName;
     });
 });
 it("should retrieve CDC/GRC for a given postal code", () => {
@@ -25,13 +26,26 @@ it("should retrieve CDC/GRC for a given postal code", () => {
     .catch(err => {
       console.error(err);
       should.not.exist(err);
-      done();
     })
     .then(data => {
       should.exist(data);
       should.exist(data.cdc);
+      data.cdc.should.not.equal("");
       should.exist(data.grc);
+      data.grc.should.not.equal("");
       should.exist(data.div);
-      done();
+      data.div.should.not.equal("");
+      Object.assign(compData, data);
     });
 });
+it("should quickly retrieve cached info", () => {
+  return grc
+    .allDataFor(119077)
+    .catch(err => {
+      console.error(err);
+      should.not.exist(err);
+    })
+    .then(allData => {
+      allData.should.deepEqual(compData);
+    });
+}).timeout(100);
